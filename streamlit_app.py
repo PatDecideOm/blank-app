d99e6bbda32f11297f5da33f49c40df269ff5a75
Patrice DecideOm <67409513+PatDecideOm@users.noreply.github.com>
2024-09-10 12:37:19 +0000

    Second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,27 @@ st.title("🎈 My new application")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+import numpy as np
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    # np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [50, 50] + [48.86, 2.33],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
